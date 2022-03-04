6f302783faa898b73257cc12bd853f571bfcbf79
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-04 17:15:58 -0300

    Adding new directive to the new component and directive to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <pm-product-list></pm-product-list>
+  </div>
+
+  `
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+
+export class AppComponent{
+  pageTitle: string = "Gerenciador de Produtos"
 }

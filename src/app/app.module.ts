71091ad83e6de243537374fe4031c51b3074e6b4
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-05 11:21:57 -0300

    Adding imports and usage for the new pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ConvertsToSpacePipe } from './shared/converts-to-space.pipe';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +9,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertsToSpacePipe
   ],
   imports: [
     BrowserModule,

297c96ab970b824d2c61f33e51d2b36035fefd8e
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-05 12:00:04 -0300

    Adding method of filter products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit {
   imageWidth = 50;
   imageMargin : number = 2;
   showImage: boolean = false;
-  listFilter: string = "cart";
+  private _listFilter: string = '';
+  get listFilter():string {
+    return this._listFilter;
+  }
+  set listFilter(value : string){
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+  filteredProducts: IProduct[] = [];
+
   pageTitle: string = 'Lista de produtos';
   products: IProduct[] = [{
     "productId": 1,
@@ -38,9 +47,16 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage;
   }
 
+  performFilter(filterBY: string): IProduct[] {
+    filterBY = filterBY.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBY));
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this._listFilter = "cart";
   }
 
 }

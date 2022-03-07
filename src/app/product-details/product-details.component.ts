2af8358963f8e398e56987d6298a66aa6cd7649e
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-07 17:06:36 -0300

    Initialize routing to product detail component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../product-list/product';
 
 @Component({
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
+  pageTitle: string = 'Informações do produto';
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
   }
 
 }

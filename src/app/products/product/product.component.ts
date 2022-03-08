b2415f74d4d6c74e80a906ee29d82323dfbfddde
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-08 11:58:01 -0300

    Creating a form template driven

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Product } from '../product';
 
 @Component({
   selector: 'pm-product',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  product = new Product();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  save(productForm: NgForm): void {
+    console.log(productForm.form);
+    console.log('Saved: ' + JSON.stringify(productForm.value));
   }
 
 }

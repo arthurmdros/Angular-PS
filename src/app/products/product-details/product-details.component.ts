1c25db22043ed90443e938ebdc1f855511b6f925
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-09 21:51:45 -0300

    Adding logic to show detail of a product

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from '../entity';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   templateUrl: './product-details.component.html',
@@ -8,13 +9,26 @@ import { IProduct } from '../entity';
 })
 export class ProductDetailsComponent implements OnInit {
   pageTitle: string = 'Informações do produto';
+  errorMessage = '';
   product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private productService: ProductsService) { }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const param =  this.route.snapshot.paramMap.get('id');
+    if(param) {
+      const id =+ param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number): void{
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void{

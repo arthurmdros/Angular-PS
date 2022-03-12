32e16ccd67666c6bfd0e370bc325b829e695c193
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-12 17:35:43 -0300

    Adding logic to acess data in API with service calls

--- src/app/shared/pdf-generator/pdf-generator.component.ts
+++ src/app/shared/pdf-generator/pdf-generator.component.ts
@@ -1,72 +1,37 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import { ProductsService } from '../../services/products.service';
+import { Subscription } from 'rxjs';
+import { IProduct } from '../../products/entity';
 
 @Component({
   selector: 'pm-pdf-generator',
   templateUrl: './pdf-generator.component.html',
   styleUrls: ['./pdf-generator.component.css']
 })
-export class PdfGeneratorComponent {
+export class PdfGeneratorComponent implements OnInit, OnDestroy {
   @ViewChild('htmlData') htmlData!: ElementRef;
 
-  PRODUCTS = [
-    {
-      "Id": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-001",
-      "releaseDate": "March 19, 2018",
-      "description": "Leaf rake with 48-inch wooden handle",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png",
-      "tags": ["rake", "leaf", "yard", "home"]
-    },
-    {
-      "Id": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-002",
-      "releaseDate": "March 18, 2018",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "Id": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-004",
-      "releaseDate": "May 21, 2018",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png",
-      "tags": ["tools", "hammer", "construction"]
-    },
-    {
-      "Id": 8,
-      "productName": "Saw",
-      "productCode": "TBX-002",
-      "releaseDate": "May 15, 2018",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "Id": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-004",
-      "releaseDate": "October 15, 2018",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
+  sub!: Subscription;
+  products: IProduct[] = [];
+  errorMessage: string = '';
 
-  constructor() {}
+  constructor(private productService:ProductsService) { }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+  
   public openPDF(): void {
     let DATA: any = document.getElementById('htmlData');
     html2canvas(DATA).then((canvas) => {
@@ -76,7 +41,7 @@ export class PdfGeneratorComponent {
       let PDF = new jsPDF('p', 'mm', 'a4');
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-      PDF.save('angular-demo.pdf');
+      PDF.save('relatorio.pdf');
     });
   }
 }

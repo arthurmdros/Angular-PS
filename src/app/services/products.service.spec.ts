9cb8ea37e8e90ac91a713d5bc9643fb5b12626ec
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-17 11:02:01 -0300

    Criação de testes para os serviços utilizados

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,16 +1,93 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing'
 
 import { ProductsService } from './products.service';
+import { IProduct } from '../products/entity';
+import { HttpErrorResponse } from '@angular/common/http';
 
-describe('ProductsService', () => {
-  let service: ProductsService;
+describe('ProductsService Tests', () => {
+  let productService: ProductsService;
+  let httpTestingController: HttpTestingController
 
+  let testProducts: IProduct[] = [
+    {
+      id: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2018',
+      description: 'Leaf rake with 48-inch wooden handle',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+      tags: ['rake', 'leaf', 'yard', 'home'],
+      isActive: true,
+    },
+    {
+      id: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2018',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+      isActive: true,
+    },
+    {
+      id: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2018',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+      tags: ['tools', 'hammer', 'construction'],
+      isActive: true,
+    }];
+
+  // CONFIGURAÇÃO INICIAL DO TESTE
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService]
+    });
+    productService = TestBed.get(ProductsService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  // EXECUÇÃO FINAL APÓS OS TESTES
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should GET all products', () => {
+    productService.getProducts()
+      .subscribe((data: IProduct[]) => {
+        expect(data.length).toBe(3)
+      });
+
+    let productsRequest: TestRequest = httpTestingController.expectOne('api/products');
+
+    expect(productsRequest.request.method).toEqual('GET');
+
+    productsRequest.flush(testProducts);
+  });
+
+  it('should return a HttpErrorResponse', () => {
+    productService.getProducts()
+    .subscribe(
+      (data: IProduct[]) => fail('deve retornar um erro'),
+      (err: HttpErrorResponse) => {
+        expect(err.message).toEqual(`${err.error.message}`)
+      }
+    );
+
+    let productsRequest: TestRequest = httpTestingController.expectOne('api/products');
+
+    productsRequest.flush('error', {
+      status: 500,
+      statusText: 'Server error'
+    });
   });
 });

6535458ec4dc65a516b51c625176c9381b32b505
arthurmdros <arthurmedeiros2005@gmail.com>
2022-07-07 13:32:15 -0300

    Componente de navegação

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class AppNavigationComponent {
+  @Input() pageTitle : string
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}

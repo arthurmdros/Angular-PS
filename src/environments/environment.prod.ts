34a9349599c77f9df1fd7ee664a72803b480956b
arthurmdros <arthurmedeiros2005@gmail.com>
2022-07-07 13:34:30 -0300

    Configuração recaptcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  recaptcha: {
+    siteKey: '6LeEXp0gAAAAANwVAqKS7MmLRkS_yYDPpHX3zk3F',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  recaptcha: {
+    siteKey: '6LeEXp0gAAAAANwVAqKS7MmLRkS_yYDPpHX3zk3F',
+  }
 };
 
 /*

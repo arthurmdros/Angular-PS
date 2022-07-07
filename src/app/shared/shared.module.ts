352659a98c6fb5cade3e6a3229e7166f12fd815d
arthurmdros <arthurmedeiros2005@gmail.com>
2022-07-07 13:33:12 -0300

    Componente slider

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,26 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { SwiperModule } from "swiper/angular";
 
 import { PaginatorComponent } from './paginator/paginator.component';
 import { PdfGeneratorComponent } from './pdf-generator/pdf-generator.component';
+import { SliderComponent } from './slider/slider.component';
 import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
     StarComponent,
     PdfGeneratorComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    SwiperModule
   ],
   exports: [
     StarComponent,
     CommonModule,
     FormsModule,
+    SliderComponent,
     PaginatorComponent,
     MatButtonModule,
     MatIconModule
---
+++ src/app/shared/slider/slider.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+@Component({
+  selector: 'slider',
+  templateUrl: 'slider.component.html',
+  styleUrls: ['slider.component.css']
+})
+export class SliderComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
+}
